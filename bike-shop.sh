37597fbabed0999e75733753fca402ab9b8cb320
HossamKhir <h.khair95@outlook.com>
2022-12-28 20:38:16 +0000

    Save progress: 1645.1

--- bike-shop.sh
+++ bike-shop.sh
@@ -173,6 +173,7 @@ RETURN_MENU() {
         # if input not rented
         if [[ -z $RENTAL_ID ]]; then
           # send to main menu
+          MAIN_MENU 'You do not have that bike rented.'
         fi
       fi
     fi
