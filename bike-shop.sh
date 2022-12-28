6c471609d73f1e93cd2c440eabcb48abf9302e44
HossamKhir <h.khair95@outlook.com>
2022-12-28 19:04:00 +0000

    Save progress: 1115.1

--- bike-shop.sh
+++ bike-shop.sh
@@ -1,65 +1,56 @@
 #!/bin/bash
 
-# PSQL="psql -X --username=freecodecamp --dbname=bikes --tuples-only -c";
-PSQL="psql -X --tuples-only bikes freecodecamp -c";
+PSQL="psql -X --username=freecodecamp --dbname=bikes --tuples-only -c"
 
 echo -e "\n~~~~~ Bike Rental Shop ~~~~~\n"
 
-MAIN_MENU(){
+MAIN_MENU() {
   if [[ $1 ]]
   then
-    echo -e "\n$1";
+    echo -e "\n$1"
   fi
-  echo "How may I help you?";
-  echo -e "\n1. Rent a bike\n2. Return a bike\n3. Exit";
+
+  echo "How may I help you?" 
+  echo -e "\n1. Rent a bike\n2. Return a bike\n3. Exit"
   read MAIN_MENU_SELECTION
+
   case $MAIN_MENU_SELECTION in
-  1)
-    RENT_MENU
-    ;;
-  2)
-    RETURN_MENU
-    ;;
-  3)
-    EXIT
-    ;;
-  *)
-    MAIN_MENU "Please enter a valid option."
-    ;;
+    1) RENT_MENU ;;
+    2) RETURN_MENU ;;
+    3) EXIT ;;
+    *) MAIN_MENU "Please enter a valid option." ;;
   esac
 }
 
-RENT_MENU(){
+RENT_MENU() {
   # get available bikes
-  AVAILABLE_BIKES=$($PSQL "SELECT bike_id, type, size FROM bikes WHERE available = true ORDER BY bike_id;");
-  QUERY="SELECT bike_id,
-    type,
-    size
-  FROM bikes
-  WHERE available = true
-  ORDER BY bike_id;";
-  AVAILABLE_BIKES=$($PSQL "$QUERY");
+  AVAILABLE_BIKES=$($PSQL "SELECT bike_id, type, size FROM bikes WHERE available = true ORDER BY bike_id")
+  echo "$AVAILABLE_BIKES"
+
   # if no bikes available
   if [[ -z $AVAILABLE_BIKES ]]
   then
     # send to main menu
-    MAIN_MENU "Sorry, we don't have any bikes available right now.";
+    MAIN_MENU "Sorry, we don't have any bikes available right now."
   else
     # display available bikes
     echo -e "\nHere are the bikes we have available:"
-  echo "$AVAILABLE_BIKES";
+
     # ask for bike to rent
+
     # if input is not a number
-      # send to main menu
+
+    # send to main menu
+
   fi
 }
 
 RETURN_MENU() {
-  echo "Return Menu";
+  echo "Return Menu"
 }
 
 EXIT() {
-  echo -e "\nThank you for stopping in.\n";
+  echo -e "\nThank you for stopping in.\n"
 }
 
 MAIN_MENU
