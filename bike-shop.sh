69623f9fffd7c65e8fd6771277295dc05761f934
HossamKhir <h.khair95@outlook.com>
2022-12-28 18:54:50 +0000

    Save progress: 980.1

--- bike-shop.sh
+++ bike-shop.sh
@@ -32,6 +32,14 @@ MAIN_MENU(){
 RENT_MENU(){
   # get available bikes
   AVAILABLE_BIKES=$($PSQL "SELECT bike_id, type, size FROM bikes WHERE available = true ORDER BY bike_id;");
+  echo "$AVAILABLE_BIKES";
+  QUERY="SELECT bike_id,
+    type,
+    size
+  FROM bikes
+  WHERE available = true
+  ORDER BY bike_id;";
+  AVAILABLE_BIKES=$($PSQL "$QUERY");
   # if no bikes available
     # send to main menu
 }
