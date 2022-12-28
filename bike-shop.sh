d487f8f2f0a39e184ff0e83cbb9f3903a150448b
HossamKhir <h.khair95@outlook.com>
2022-12-28 19:31:36 +0000

    Save progress: 1236.1

--- bike-shop.sh
+++ bike-shop.sh
@@ -72,6 +72,11 @@ RENT_MENU() {
       if [[ -z $BIKE_AVAILABILITY ]]; then
         # send to main menu
         MAIN_MENU "That bike is not available."
+      else
+        # get customer info
+        # if customer doesn't exist
+        # get new customer name
+        # insert new customer
       fi
     fi
 
